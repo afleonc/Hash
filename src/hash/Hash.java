aeb52f5437a4edd50de6333aaba56b777bc1dacc
blzK <bpertev@gmail.com>
2013-10-10 23:02:22 +0200

    Update Hash.java

--- src/hash/Hash.java
+++ src/hash/Hash.java
@@ -88,74 +88,7 @@ public class Hash {
     }
     
             
-            /*
-    private int testElement(String newChain) {
-        //Méthode testElement avec l'opérateur %
-        int i;
-        int startPoint;
-        if (newChain != null) {
-            i = newChain.hashCode() % t.length;
-            startPoint = newChain.hashCode() % t.length;
-        } else {
-            i = 0;
-            startPoint = 0;
-        }
-        while (t[i] != newChain) {
-            i = (i + 1) % t.length;
-            if (i == startPoint) {
-                return -1;
-            }
-            
-        }
-        
-        return i;
-    }
-    
-        private int testElement2(String newChain) {
-        //Méthode testElement avec l'opération bit
-        int i;
-        int startPoint;
-        if (newChain != null) {
-            i = newChain.hashCode() & (t.length - 1);//newChain.hashCode() % t.length;
-
-            //x % 2n == x & (2n - 1)
-            startPoint = newChain.hashCode() & (t.length - 1);
-        } else {
-            i = 0;
-            startPoint = 0;
-        }
-        while (t[i] != newChain) {
-            i = (i + 1) & (t.length - 1);
-            if (i == startPoint) {
-                return -1;
-            }
-
-        }
-
-        return i;
-    }*/
-    /*
-    public boolean contains2(String newChain) {
-        
-        if (testElement2(newChain) == -1) {
-            return false;
-        } else {
-            return true;
-        }
-        
-    }
-    
-    public void add2(String newChain) {
-        int a = testElement2(null);
-        //On vérifie qu'il y ait de la place
-        int b = testElement2(newChain);
-        //On vérfifie que notre liste ne contient pas l'élément
-        if (a != -1 & b == -1) {
-            this.t[a] = newChain;
-        }
-
-    }
-*/
+     
     public Hash dump() {
         int i = 0;
         while (i < this.t.length) {
